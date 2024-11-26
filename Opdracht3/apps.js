f484ba636e10d4d4f06569be3b71846ff79f11d2
Margarida <9019412@student.zadkine.nl>
2024-11-26 13:09:43 +0100

    Add files via upload

---
+++ Opdracht3/apps.js
@@ -0,0 +1,9 @@
+for(let i = 0; i <=100; i++){
+   let h1test = document.getElementById('test');
+   h1test.innerText += i + "_";
+}
+ 
+for(let m = 100; m >=0; m--){
+    let h2test = document.getElementById('test2');
+    h2test.innerText += m + "_";
+ }
